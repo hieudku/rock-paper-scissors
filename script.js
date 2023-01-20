72a708149c6718846e8fbc5e68129e6e5c051f98
Hieu Cu <hieu.dcu@gmail.com>
2023-01-21 01:44:54 +1300

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+    const playerSelection = prompt("what is your move?");
+    const arr = ['rock','paper','scissor']
+    alert(getRandom(arr));
+
+function winner(playerSelection, computerSelection) {
+    getRandom();
+}
+        console.log(playerSelection, getRandom(arr));
