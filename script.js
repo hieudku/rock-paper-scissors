fb690b0e493f0af37b320eb4feac03f649a3786c
Hieu Cu <hieu.dcu@gmail.com>
2023-02-02 00:54:48 +1300

    Changed some text outputs

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ let playerChoice = document.querySelectorAll('button');
     button.addEventListener('click' , () => {
         let playerSelection = button.id;
         playerChoice = playerSelection;
-            document.getElementById("playerChoiceDisplay").innerHTML = "You chose: " + playerChoice;
+            document.getElementById("playerChoiceDisplay").innerHTML = "You chose " + playerChoice;
 
         let computerSelection = getRandom()
         computerChoice = computerSelection;
-            document.getElementById("computerChoiceDisplay").innerHTML = "Computer chose: " + computerChoice;
+            document.getElementById("computerChoiceDisplay").innerHTML = "AI chose " + computerChoice;
         outcome(playerChoice, computerChoice);
         scoreCount();
             document.getElementById("playerScore").innerHTML = "Your score: " + playerWinsTally;
-            document.getElementById("computerScore").innerHTML = "Computer's score: " + computerWinsTally;
+            document.getElementById("computerScore").innerHTML = "AI's score: " + computerWinsTally;
         winnerLoser(playerWinsTally, computerWinsTally);
     });
 });
 
 function getRandom(arr) { //Choose random from array.
-    arr = ['rock','paper','scissor'];
+    arr = ['rock','paper','scissors'];
     return arr[Math.floor(Math.random() * arr.length)];
 }
 function outcome(playerChoice, computerChoice) {
@@ -28,7 +28,7 @@ function outcome(playerChoice, computerChoice) {
             document.getElementById("outCome").innerHTML = "It's a draw!";
             return 'draw';
         }
-            else if (playerChoice == 'rock' && computerChoice == 'scissor') {  
+            else if (playerChoice == 'rock' && computerChoice == 'scissors') {  
                 document.getElementById("outCome").innerHTML = "You win this round!";
                     return 'playerWins';
             }
@@ -36,12 +36,12 @@ function outcome(playerChoice, computerChoice) {
                     document.getElementById("outCome").innerHTML = "You win this round!";
                     return 'playerWins';
             }
-                else if(playerChoice == 'scissor' && computerChoice == 'paper') { 
+                else if(playerChoice == 'scissors' && computerChoice == 'paper') { 
                     document.getElementById("outCome").innerHTML = "You win this round!";
                     return 'playerWins';
             }
                     else {
-                        document.getElementById("outCome").innerHTML = "Computer wins this round!";
+                        document.getElementById("outCome").innerHTML = "AI wins this round!";
                         return 'computerWins';
                     }
 }
@@ -63,7 +63,7 @@ function winnerLoser(playerWinsTally, computerWinsTally) { //Compare and decide 
         return 'You wins!'
     }
         else if (computerWinsTally == 5) {
-            document.getElementById("finalResult").innerHTML = "COMPUTER WON! Humanity has been defeated by AI!";
+            document.getElementById("finalResult").innerHTML = "AI WON! Can I have some fries with that?";
             return 'Computer wins!'
         }
         else if (playerWinsTally > computerWinsTally) {
@@ -71,11 +71,11 @@ function winnerLoser(playerWinsTally, computerWinsTally) { //Compare and decide 
             return 'You are leading!'
         }
         else if(computerWinsTally > playerWinsTally) {
-            document.getElementById("finalResult").innerHTML = "Computer is leading! Go AI!";
+            document.getElementById("finalResult").innerHTML = "AI is leading! Go AI!";
             return 'Computer is leading!'
         }
             else if (playerWinsTally == computerWinsTally) {
-                document.getElementById("finalResult").innerHTML = "DRAW! Might as well flip a coin!";
+                document.getElementById("finalResult").innerHTML = "DRAW! Might as well flip a coin..";
                 return 'Draws!'
             }
 }
